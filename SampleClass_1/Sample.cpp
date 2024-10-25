4e01f94ac4f98e525e63c43246d3fb3be1c4a883
prokvip <prokvip@msn.com>
2024-10-25 11:44:56 +0900

    1025_2

---
+++ SampleClass_1/Sample.cpp
@@ -0,0 +1,142 @@
+﻿#include "TStudentManager.h"
+#define CLASS_FUNCTION
+#ifndef CLASS_FUNCTION
+void (TStudentManager::*RUN)(TLinkedlist& list);
+int main()
+{
+    TStudentManager mgr;      
+
+    int iWork = 0;
+    bool workRun = true;
+    while (workRun)
+    {        
+        mgr.Title();
+        printf("\nPRINT(0)NEW_DATA(1)SAVE(2)LOAD(3)UPDATE(4)FIND(5)DEL(6)SORT(7)SAMPLE(8)EXIT(99)");
+        scanf_s("%d", &iWork);
+        
+        switch (iWork)
+        {
+            case SAMPLE_DATA:
+            {
+                //TStudentManager::SampleData': 비표준 구문입니다. '&'를 사용하여 멤버 포인터를 만드세요.
+                RUN = &TStudentManager::SampleData;
+            }break;
+            case PRINT:
+            {
+                RUN = &TStudentManager::PrintData;
+            }break;
+            case NEW_DATA:
+            {
+                RUN = &TStudentManager::NewData;
+            }break;
+            case FILE_SAVE:
+            {
+                RUN = &TStudentManager::FileSave;
+            }break;
+            case FILE_LOAD:
+            {
+                RUN = &TStudentManager::FileLoad;
+            }break;
+            case UPDATE:
+            {
+                RUN = &TStudentManager::UpdateData;
+            }break;
+            case FIND:
+            {
+                RUN = &TStudentManager::FindData;
+            }break;
+            case DELETE:
+            {
+                RUN = &TStudentManager::DeleteData;
+            }break;
+            case SORT:
+            {
+                RUN = &TStudentManager::SortData;
+            }break;
+            case EXIT:
+            {
+                RUN = nullptr;
+                workRun = false;
+            }break;
+        }
+        
+        if (RUN != nullptr)
+        {            
+            //RUN(mgr.GetList()); // error
+            (mgr.*RUN)(mgr.GetList());
+        }
+    }
+}
+#else
+int main()
+{
+    TStudentManager mgr;   
+    ////mgr.Run();
+    //
+    //bool workRun = true;
+    //while (workRun)
+    //{
+    //    workRun = mgr.Run();
+    //}
+    int iWork = 0;
+    bool workRun = true;
+    while (workRun)
+    {
+        mgr.Title();
+        printf("\nPRINT(0)NEW_DATA(1)SAVE(2)LOAD(3)UPDATE(4)FIND(5)DEL(6)SORT(7)SAMPLE(8)EXIT(99)");
+        scanf_s("%d", &iWork);
+
+        switch (iWork)
+        {
+        case SAMPLE_DATA:
+        {
+            //TStudentManager::SampleData': 비표준 구문입니다. '&'를 사용하여 멤버 포인터를 만드세요.
+            mgr.EXECUTE = &TStudentManager::SampleData;
+        }break;
+        case PRINT:
+        {
+            mgr.EXECUTE = &TStudentManager::PrintData;
+        }break;
+        case NEW_DATA:
+        {
+            mgr.EXECUTE = &TStudentManager::NewData;
+        }break;
+        case FILE_SAVE:
+        {
+            mgr.EXECUTE = &TStudentManager::FileSave;
+        }break;
+        case FILE_LOAD:
+        {
+            mgr.EXECUTE = &TStudentManager::FileLoad;
+        }break;
+        case UPDATE:
+        {
+            mgr.EXECUTE = &TStudentManager::UpdateData;
+        }break;
+        case FIND:
+        {
+            mgr.EXECUTE = &TStudentManager::FindData;
+        }break;
+        case DELETE:
+        {
+            mgr.EXECUTE = &TStudentManager::DeleteData;
+        }break;
+        case SORT:
+        {
+            mgr.EXECUTE = &TStudentManager::SortData;
+        }break;
+        case EXIT:
+        {
+            mgr.EXECUTE = nullptr;
+            workRun = false;
+        }break;
+        }
+
+        if (mgr.EXECUTE != nullptr)
+        {
+            //RUN(mgr.GetList()); // error            
+            mgr.Execute(mgr.GetList());
+        }
+    }
+}
+#endif
---
+++ SampleClass_1/TStudentManager.cpp
@@ -0,0 +1,197 @@
+#include "TStudentManager.h"
+void* TStudentManager::g_pValueData = nullptr;
+void  TStudentManager::PrintData(TLinkedlist& list) {
+    system("cls");
+    list.ShowAll(false, ShowStudent);
+}
+void  TStudentManager::NewData(TLinkedlist& list)
+{
+    char name[4] = { 0, };
+    int iID, iKor, iEng, iMat;
+    printf("\n이름,번호,국어,영어,수학 : ");
+    scanf_s("%s %d %d %d %d", name, _countof(name), &iID, &iKor, &iEng, &iMat);
+    TNode* pNewNode = CreateStudent(iID, name, iKor, iEng, iMat);
+    if (pNewNode != nullptr)
+    {
+        pNewNode->iKey = ++TNode::iKeyIndex;
+        list.push_back(pNewNode);
+    }
+}
+void  TStudentManager::FileSave(TLinkedlist& list)
+{
+    FILE* fp = 0;
+    fopen_s(&fp, "data.txt", "w");
+    fprintf(fp, "%d %d\n", list.GetCounter(), TNode::iKeyIndex);
+    list.ShowAll(false, ShowStudent, fp);
+    fclose(fp);
+}
+void  TStudentManager::FileLoad(TLinkedlist& list)
+{
+    FILE* fp = 0;
+    fopen_s(&fp, "data.txt", "r");
+    if (fp != nullptr)
+    {
+        list.DeleteAll();
+    }
+
+    char buffer[256] = { 0 , };
+    int iNodeCounter = 3;
+    fgets(buffer, 256, fp);
+    sscanf_s(buffer, "%d %d", &iNodeCounter, &TNode::iKeyIndex);
+
+    //fscanf_s(fp, "%d %d\n",&iNodeCounter, &TNode::iKeyIndex);
+    for (int iNode = 0; iNode < iNodeCounter; iNode++)
+    {
+        char name[4] = { 0, };
+        int iKor, iEng, iMat, iTotal;
+        float fAvg;
+
+        int id, iKey;
+
+        fgets(buffer, 256, fp);
+        sscanf_s(buffer, "%d %d %s %d %d %d\n", &iKey, &id, name, _countof(name), &iKor, &iEng, &iMat, &iTotal, &fAvg);
+        TNode* pNewNode1 = CreateStudent(id, name, iKor, iEng, iMat);
+        pNewNode1->iKey = iKey;
+        if (pNewNode1 != nullptr)
+        {
+            list.push_back(pNewNode1);
+        }
+    }
+    list.ShowAll(false, ShowStudent, fp);
+    fclose(fp);
+};
+void  TStudentManager::UpdateData(TLinkedlist& list) {
+    int iIndex;
+    printf("index= ");
+    scanf_s("%d", &iIndex);
+    TNode* pNode = list.Find(iIndex);
+    ShowStudent(pNode);
+
+    printf("\n이름,번호,국어,영어,수학 : ");
+    scanf_s("%s %d %d %d %d", pNode->data.m_szName, _countof(pNode->data.m_szName),
+        &pNode->data.m_iIndex, &pNode->data.m_iKor, &pNode->data.m_iEng, &pNode->data.m_iMat);
+
+    pNode->data.m_iTotal =
+        pNode->data.m_iKor +
+        pNode->data.m_iEng +
+        pNode->data.m_iMat;
+    pNode->data.m_fAverage = pNode->data.m_iTotal / 3.0f;
+}
+void  TStudentManager::FindData(TLinkedlist& list)
+{
+    char name[4] = { 0, };
+    scanf_s("%s", name, _countof(name));
+    g_pValueData = name;
+    TNode* pNode = list.Find(FindStudentString);
+    if (pNode != nullptr)
+    {
+        ShowStudentLine(pNode);
+    }
+}
+void  TStudentManager::DeleteData(TLinkedlist& list)
+{
+    int iIndex;
+    printf("\nindex= ");
+    scanf_s("%d", &iIndex);
+    TNode* pNode = list.Find(iIndex);
+    ShowStudent(pNode);
+
+    int iDelete = 0;
+    printf("\n%d 노드를 삭제하시겠습니까.y(1)or n(0)", iIndex);
+    scanf_s("%d", &iDelete);
+    if (iDelete == 1)
+    {
+        list.Erase(pNode);
+        printf("\n%d 삭제되었습니다.", iIndex);
+    }
+}
+void  TStudentManager::SortData(TLinkedlist& list)
+{
+    /*TNode* a = g_pHead->pNext;
+    TNode* b = g_pHead->pNext->pNext->pNext;
+    Swap(a, b);*/
+
+    /*  TNode* a = g_pHead->pNext;
+      TNode* b = g_pHead->pNext->pNext;
+      Swap(a, b);*/
+
+    list.Sort(AscendingStudent);
+    //list.Sort();
+}
+void  TStudentManager::SampleData(TLinkedlist& list)
+{
+    for (int iNode = 0; iNode < 10; iNode++)
+    {
+        TNode* pNewNode = CreateStudent(iNode);
+        if (pNewNode != nullptr)
+        {
+            pNewNode->iKey = ++TNode::iKeyIndex;
+            list.push_back(pNewNode);
+        }
+    }
+}
+
+bool TStudentManager::Run()
+{
+    int iWork = 0;
+    bool workRun = true;
+    //while (workRun)
+    {
+        Title();
+        printf("\nPRINT(0)NEW_DATA(1)SAVE(2)LOAD(3)UPDATE(4)FIND(5)DEL(6)SORT(7)SAMPLE(8)EXIT(99)");
+        scanf_s("%d", &iWork);
+
+        switch (iWork)
+        {
+        case SAMPLE_DATA:
+        {
+            //TStudentManager::SampleData': 비표준 구문입니다. '&'를 사용하여 멤버 포인터를 만드세요.
+            EXECUTE = &TStudentManager::SampleData;
+        }break;
+        case PRINT:
+        {
+            EXECUTE = &TStudentManager::PrintData;
+        }break;
+        case NEW_DATA:
+        {
+            EXECUTE = &TStudentManager::NewData;
+        }break;
+        case FILE_SAVE:
+        {
+            EXECUTE = &TStudentManager::FileSave;
+        }break;
+        case FILE_LOAD:
+        {
+            EXECUTE = &TStudentManager::FileLoad;
+        }break;
+        case UPDATE:
+        {
+            EXECUTE = &TStudentManager::UpdateData;
+        }break;
+        case FIND:
+        {
+            EXECUTE = &TStudentManager::FindData;
+        }break;
+        case DELETE:
+        {
+            EXECUTE = &TStudentManager::DeleteData;
+        }break;
+        case SORT:
+        {
+            EXECUTE = &TStudentManager::SortData;
+        }break;
+        case EXIT:
+        {
+            EXECUTE = nullptr;
+            workRun = false;
+        }break;
+        }
+
+        if (EXECUTE != nullptr)
+        {            
+            Execute(GetList());
+            //(this->*Execute)
+        }
+    }
+    return workRun;
+}
