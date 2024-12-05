dcafa0da87f35d338a9109cd7995e8e9e12c8858
prokvip <prokvip@msn.com>
2024-12-05 10:53:41 +0900

    1205_1

---
+++ CreateObject_0/TObject.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "TVertex.h"
+class TObject
+{
+	ID3D11Buffer*		m_pVertexBuffer=nullptr;	
+	ID3D11VertexShader* m_pVertexShader = nullptr;
+	ID3DBlob*			m_pCode;
+	ID3D11PixelShader*  m_pPixelShader = nullptr;
+	ID3D11InputLayout*  m_pInputLayout = nullptr;
+
+	std::vector<PC_VERTEX>    m_vVertexList;
+public:
+	virtual bool	Create() ; // 持失
+		virtual bool	CreateVertexBuffer(); // 持失
+		virtual bool	CreateVertexShader(); // 持失
+		virtual bool	CreatePixelShader(); // 持失
+		virtual bool	CreateInputLayout(); // 持失
+	virtual void    SetVertexData();
+public:
+	virtual void	Init();
+	virtual void	Frame();
+	virtual void	Render();
+	virtual void	Release();
+};
+
+class TObject2D : public TObject
+{
+public:
+	virtual bool	Create() override; // 持失
+	virtual void	Init()override;
+	virtual void	Frame()override;
+	virtual void	Render() override;
+	virtual void	Release()override;
+};
---
+++ CreateObject_0/TStd.h
@@ -0,0 +1,60 @@
+#pragma once
+#include <windows.h>
+#include <d3d11.h>
+#include <d3dcompiler.h>
+#include <tchar.h> // _T("") == L""
+#include <string>
+#include <vector>
+#include <list>
+#include <map>
+#include <algorithm>
+#pragma comment(lib,"d3d11.lib") // 加己 眠啊辆加己 
+#pragma comment(lib,"d3dcompiler.lib") // 加己 眠啊辆加己 
+
+
+
+struct TGameKey
+{
+	//DWORD frontMove;// w or VK_UP
+	//DWORD Attack;// Lbutton or VK_ENTER
+	DWORD dwWkey;
+	DWORD dwSkey;
+	DWORD dwAkey;
+	DWORD dwDkey;
+	DWORD dwLeftClick;
+	DWORD dwRightClick;
+	DWORD dwMiddleClick;
+	DWORD dwExit;
+	DWORD dwSpace;
+};
+
+extern POINT g_WindowSize;
+extern HWND  g_hWnd;
+extern TGameKey g_GameKey;
+extern float   g_fSPF;
+extern float   g_fGT;
+
+static void DX_CHECK(HRESULT hr, const TCHAR* function)
+{
+    if (FAILED(hr))
+    {
+        LPWSTR output;
+        WCHAR buffer[256] = { 0, };
+        FormatMessage(
+            FORMAT_MESSAGE_FROM_SYSTEM |
+            FORMAT_MESSAGE_IGNORE_INSERTS |
+            FORMAT_MESSAGE_ALLOCATE_BUFFER,
+            NULL, hr,
+            MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPTSTR)&output, 0, NULL);
+        wsprintf(buffer, L"File=%ls\nLine=%d\nFuction=%ls", _T(__FILE__), __LINE__, function);
+        std::wstring msg = buffer;
+        msg += L"\n";
+        msg += output;
+
+        std::wstring title = L"ERROR(";
+        title += std::to_wstring(hr);
+        title += L")";
+        MessageBox(NULL, msg.c_str(),
+            title.c_str(), MB_OK);
+    }
+}
