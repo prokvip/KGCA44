35c317d7bb79cd9756c44c3fc3e1f3d5a537441f
prokvip <prokvip@msn.com>
2025-02-04 11:08:20 +0900

    TCore lib

---
+++ TNetWindow/Sample.h
@@ -0,0 +1,10 @@
+#pragma once
+//빌드 후 이벤트
+//copy "*.h" "..\\..\\TCore\\Inc"
+//copy "..\\..\\output\\debug\\TCore.lib" "..\\..\\TCore\\lib\\"
+
+#include "TWindow.h"
+class Sample : public TWindow
+{
+};
+
