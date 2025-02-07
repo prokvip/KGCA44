0ae2c0cd8ab889c40cfaf7e0f706920201e58f09
prokvip <prokvip@msn.com>
2025-02-07 12:06:21 +0900

    0207_1

---
+++ udpBroadcast/udpBroadcast.cpp
@@ -0,0 +1,30 @@
+﻿#include <winsock2.h>
+#include<iostream>
+#include<conio.h>
+using namespace std;
+#pragma comment(lib, "ws2_32.lib")
+
+int main()
+{
+	WSADATA wsaData;
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+	SOCKET sock;
+	sock = socket(AF_INET, SOCK_DGRAM, 0);
+	char broadcast = '1';
+	setsockopt(sock, SOL_SOCKET, SO_BROADCAST,
+		&broadcast, sizeof(broadcast));
+	
+	sockaddr_in BroadcastAddr;
+	BroadcastAddr.sin_family = AF_INET;
+	BroadcastAddr.sin_port = htons(10000);
+	BroadcastAddr.sin_addr.s_addr = INADDR_BROADCAST;
+
+	char msg[] = "hello";
+	sendto(sock, msg, strlen(msg), 0,
+		(sockaddr*)&BroadcastAddr, sizeof(BroadcastAddr));
+
+	_getch();
+	closesocket(sock);
+	WSACleanup();
+}
