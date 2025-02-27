cd063aa515f1fe42cbbf045b5412eb01bffc43cd
prokvip <prokvip@msn.com>
2025-02-27 13:00:09 +0900

    0227_1

--- IOCPServer/THost.cpp
+++ IOCPServer/THost.cpp
@@ -54,15 +54,17 @@ bool THost::AsyncRecvTCP()
     }
     return true;
 }
+
 void THost::Put(char* recvBuffer,	int iRecvSize)
 {
 	// 받는 버퍼의 용량이 부족하면
 	if (m_iWritePos + iRecvSize >= MAX_RECV_SIZE)
 	{
-		char strTemp[MAX_RECV_SIZE] = { 0, };
+		/*char strTemp[MAX_RECV_SIZE] = { 0, };
 		memcpy(strTemp, &m_RecvBuffer[m_iStartPos], m_iReadPos);
 		ZeroMemory(&m_RecvBuffer, sizeof(char) * MAX_RECV_SIZE);
-		memcpy(&m_RecvBuffer, &strTemp, m_iReadPos);
+		memcpy(&m_RecvBuffer, &strTemp, m_iReadPos);*/
+		memmove(m_RecvBuffer, &m_RecvBuffer[m_iStartPos], m_iReadPos);
 		m_pPacket = (P_UPACKET)m_RecvBuffer;
 		m_iStartPos = 0;
 		m_iWritePos = m_iReadPos;
@@ -87,8 +89,12 @@ void THost::Put(char* recvBuffer,	int iRecvSize)
 			m_iReadPos -= m_pPacket->ph.len;
 			// 패킷의 시작 위치
 			m_iStartPos += m_pPacket->ph.len;
+
 			// 잔여량이 없을 경우
-			if (add.ph.len == iRecvSize) break;
+			if (m_iReadPos == 0)
+			{
+				break;
+			}
 			// 잔여량이 패킷 헤더보 작을 경우
 			if (m_iReadPos < PACKET_HEADER_SIZE) break;
 
